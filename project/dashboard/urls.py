78c868d5f2253018cb5839c27cd691ca04d7916c
Extragornax <gaspardwi@yahoo.fr>
2019-12-16 14:02:22 +0100

    Init dashboard, template issues to be fixed

---
+++ project/dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+app_name = 'dashboard'
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+#     url(r'^restaurants$', views.restaurants, name='restaurants'),
+#     url(r'^restaurants/specials/(?P<restaurant>[a-z0-9 ]+)$',
+#         views.specials, name='specials'),
+]
