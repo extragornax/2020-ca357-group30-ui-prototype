d4edeaec8863cf80104176a4dac8137ea78a684f
Anthony TRAM AKA DotCheap <anthony.tram@epitech.eu>
2019-12-16 14:37:30 +0000

    Added gitignore for DCU-ui-

--- project/dashboard/urls.py
+++ project/dashboard/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import url
 from . import views
 app_name = 'dashboard'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-#     url(r'^restaurants$', views.restaurants, name='restaurants'),
-#     url(r'^restaurants/specials/(?P<restaurant>[a-z0-9 ]+)$',
-#         views.specials, name='specials'),
+    # url(r'^$', views.index, name='index'),
+    url('', views.index, name='index'),
+    #     url(r'^restaurants$', views.restaurants, name='restaurants'),
+    #     url(r'^restaurants/specials/(?P<restaurant>[a-z0-9 ]+)$',
+    #         views.specials, name='specials'),
 ]
