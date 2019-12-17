283146de2a3651c3ee23e07148246a185cd39891
Anthony TRAM AKA DotCheap <anthony.tram@epitech.eu>
2019-12-17 17:51:55 +0000

    fixes search bar for modules

--- frontend-django/dashboard/views.py
+++ frontend-django/dashboard/views.py
@@ -6,10 +6,17 @@ import requests
 # Index route
 
 
+def cleanModulesSearch(s):
+    s = s.replace(" ", "")
+    s = s.upper()
+
+    return s
+
+
 def index(request):
     context = {}
     if "modules" in request.GET:
-        context['modules'] = request.GET['modules']
+        context['modules'] = cleanModulesSearch(request.GET['modules'])
     if "announcements" in request.GET:
         context['announcements'] = request.GET['announcements']
     webservice_url = 'http://vps.cheap.appboxes.co:10126/'
@@ -22,23 +29,28 @@ def index(request):
         4: "Friday",
         5: "Saturday",
         6: "Sunday"
-	}
+    }
 
-    modulesPerDay = { 0:[], 1:[], 2:[], 3:[], 4:[], 5:[], 6:[] }
+    modulesPerDay = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
 
+    modules_error = ""
     if "modules" in context:
-        webservice_url += "?modules=" + context['modules']
+        print(str(context['modules']))
+        webservice_url += "?modules=" + cleanModulesSearch(context['modules'])
     modulesInfos = requests.get(webservice_url).json()
-
     for module in modulesInfos:
-        moduleName = module  + " - " + modulesInfos[module]['name']
-        for i in modulesInfos[module]['events']:
-            modulesPerDay[int(i)].append({
-                "begins": modulesInfos[module]['events'][i]['begins'],
-                "duration": modulesInfos[module]['events'][i]['duration'],
-                "module": moduleName
-            })
-            # print(modulesInfos[module]['events'][i]['begins'], "for", modulesInfos[module]['events'][i]['duration'])
-        # print(moduleName)
+        if modulesInfos[module] != "Not Found":
+            moduleName = module + " - " + modulesInfos[module]['name']
+            for i in modulesInfos[module]['events']:
+                modulesPerDay[int(i)].append({
+                    "begins": modulesInfos[module]['events'][i]['begins'],
+                    "duration": modulesInfos[module]['events'][i]['duration'],
+                    "module": moduleName
+                })
+        else:
+            if len(modules_error) == 0:
+                modules_error += "Some modules were not found, please note only CA modules are available please recheck : "
+            modules_error += module + " "
 
+    context['modules_error'] = modules_error
     return render(request, 'dashboard/index.html', context)
